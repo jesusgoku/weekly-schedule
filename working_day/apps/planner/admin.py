9ddace586514676b7f837484ab97e40cef624405
Jesús Urrutia <jesus.urrutia@gmail.com>
2018-09-22 00:24:15 -0300

    Added admin for WeeklySchedule and WorkingBlock models

--- working_day/apps/planner/admin.py
+++ working_day/apps/planner/admin.py
@@ -2,3 +2,16 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
+
+from working_day.apps.planner.models import WeeklySchedule, WorkingBlock
+
+
+@admin.register(WeeklySchedule)
+class WeeklyScheduleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WorkingBlock)
+class WorkingBlockAdmin(admin.ModelAdmin):
+    pass
